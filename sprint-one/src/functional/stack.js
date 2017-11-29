cf14948f9aa580a883f0f45d75b27a65cd0eb27c
Chase Lee <chaseklee990@gmail.com>
2017-11-29 11:07:37 -0800

    Implement size function property on functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    var size = 0;
+    // loop through object
+    for(var key in storage) {
+      // if object has own property
+      if(storage.hasOwnProperty(key)) {
+        // increment size
+        size++;
+      }
+    }
+    //return size
+    return size;
   };
 
   return someInstance;
