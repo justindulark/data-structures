6c3fc2d26eeb76c5831995418b5951b3a8ed1b40
justindulark <justindulark@gmail.com>
2017-11-29 20:16:03 -0800

    Implement functionality for prototypal queue, pseudoclassical queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[Object.keys(this.storage).length] = value; 
+  },
+    
+  dequeue: function() {
+    var dq = this.storage[0];
+    
+    for (var i = 0; i < this.size(); i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    
+    delete this.storage[this.size() - 1];
+    
+    return dq;
+  },
+    
+  // INPUT: n/a
+  // OUTPUT: returns a number 
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
