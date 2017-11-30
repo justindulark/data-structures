f6b92d977300fa7f746e7332ea529221c98c37b9
justindulark <justindulark@gmail.com>
2017-11-29 16:40:43 -0800

    Implement enqueue and size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  extend(someInstance, Queue.queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+Queue.queueMethods = {};
 
 
+Queue.queueMethods.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length] = value; 
+},
+  
+Queue.queueMethods.dequeue = function() {
+  var dq = Object.keys(this.storage)[0];
+  
+  delete Object.keys(this.storage)[0];
+  
+  return dq;
+},
+  
+// INPUT: n/a
+// OUTPUT: returns a number 
+Queue.queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
