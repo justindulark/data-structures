9d71eb858938ea9692f06b6580011e114ddeff2d
justindulark <justindulark@gmail.com>
2017-11-30 10:57:50 -0800

    Implement addToTail in linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,21 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // creating new node with value 
+    var node = Node(value);
+    if (list.tail) {
+      list.tail.next = node;
+      list.tail = node;
+    } else {
+      list.tail = node;
+      list.head = node;
+    }
+
+    // check if head ! exists
+    // if (!list.head) {
+    //   // set head equal to tail
+    //   list.head = node;
+    // }     
   };
 
   list.removeHead = function() {
