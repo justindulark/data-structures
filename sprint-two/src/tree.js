2d8bd8fc184dd6220e1bcaed215aa18e3aba844b
justindulark <justindulark@gmail.com>
2017-11-30 16:39:26 -0800

    Implement contains in tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; 
 
   return newTree;
 };
@@ -11,13 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children[this.children.length] = Tree(value);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target) === true) {
+      return true;
+    }    
+  }
+  
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild: O(1)
+ contains: O(n)
  */
