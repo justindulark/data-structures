bfb8645a2d7eab0703c6638151ed8c22a419dde6
justindulark <justindulark@gmail.com>
2017-12-01 18:34:12 -0800

    Implement part of insert function to pass some tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value;
+  if (!(Array.isArray(this._storage.get(index)))) {
+    this._storage.set(index, []);
+  } 
+  value = this._storage.get(index);
+  value.push([k, v]);
+  this._storage.set(index, value);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value = this._storage.get(index);
+  for (var i = 0; i < value.length; i++) {
+    if (value[i][0] === k) {
+      return value[i][1];
+    }
+  }
 };
-
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
