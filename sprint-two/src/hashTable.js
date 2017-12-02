9ddcd65855b8d120548219aadf119cea071a919c
justindulark <justindulark@gmail.com>
2017-12-01 19:11:11 -0800

    Add complexity analysis for each function in hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -52,6 +52,9 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: best case O(1), average O(n)
+ retrieve: O(n)
+ remove: O(n)
  */
 
 
