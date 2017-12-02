f2813cb9c5baaa2f7303d6a43ef87fee0407a62a
justindulark <justindulark@gmail.com>
2017-12-01 20:02:37 -0800

    Insert functions in binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,30 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree);
+  bst.left = null;
+  bst.right = null;
+  bst.value = null;
+  return bst;
 };
 
+BinarySearchTree.left = function() {
+  
+};
 
+BinarySearchTree.right = function() {
+  
+};
+
+BinarySearchTree.insert = function() {
+  
+};
+
+BinarySearchTree.contains = function() {
+  
+};
+
+BinarySearchTree.depthFirstLog = function(func) {
+  
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
